462605e0c4419f9662cce472b2b373c6da87da1a
João Lucas <jony.lucas.br13@gmail.com>
2024-10-14 16:24:10 -0300

    - Bug Fix + Observer for shooting

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -4,17 +4,31 @@ using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
+    public static Action OnShoot;
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private float _fireCooldown = 0.5f;
+    
     private Camera _mainCamera;
     private Vector2 _direction;
+    private float _fireTimer;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        OnShoot += ShootProjectile;
+    }
+    
+    private void OnDisable()
+    {
+        OnShoot -= ShootProjectile;
     }
 
     private void Update()
@@ -25,11 +39,18 @@ public class Gun : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        _fireTimer += Time.deltaTime;
+        if (Mouse.current.leftButton.isPressed && _fireTimer >= _fireCooldown)
         {
-            var newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
-            newBullet.FireDirection = _direction.normalized;
+            OnShoot?.Invoke();
         }
+    }
+
+    private void ShootProjectile()
+    {
+        var newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+        newBullet.FireDirection = _direction.normalized;
+        _fireTimer = 0;
     }
 
     private void Rotate()
